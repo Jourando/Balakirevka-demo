42fe625bd4f930faa0918dd1b0a011ab0f324fe6
Paul the Stranger <paul.ivan.stranger@gmail.com>
2017-10-01 00:11:27 +0300

    Step 3

--- www/index.php
+++ www/index.php
@@ -1,10 +1,12 @@
 <?php
 // demo //
 $lines=file('demo.a');
+echo "<HTML><HEAD><meta charset=\"utf-8\"><TITLE>INDEX</TITLE></HEAD><BODY>";
 echo "<ul>";
 for ($i=0; $i<count($lines); $i++) {
 		list($link, $descr, $addinfo)=explode('=>>', $lines[$i]);
 		echo "<li><a href=".$link.">".$link."</a> - ".$descr." (".$addinfo.")</li>";
 }
 echo "</ul>";
+echo "</BODY></HTML>";
 ?>

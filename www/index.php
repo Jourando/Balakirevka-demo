7223b83821b7a13c24885e96487142665413f996
Paul the Stranger <paul.ivan.stranger@gmail.com>
2017-10-04 14:50:18 +0300

    Step 19

--- www/index.php
+++ www/index.php
@@ -2,12 +2,22 @@
 // demo // ревизию не ставим
 $lines=file('demo.a');
 echo "<HTML lang='RU-ru'><HEAD><meta charset=\"utf-8\"><TITLE>INDEX</TITLE></HEAD><BODY>";
-echo "<h4>Список демо-файлов</h4>";
-echo "<ul>";
+echo "<Table border=0><tr><td><h4>Файлы с вариантами на выбор</h4></td>";
+echo "<td>&nbsp;</td><td><h4>Файлы с инструкциями</h4></td></tr>";
+echo "<tr><td><ul>";
 for ($i=0; $i<count($lines); $i++) {
 		list($link, $descr, $addinfo)=explode('==>', $lines[$i]);
 		echo "<li><a href=".trim($link).">".$link."</a> - ".trim($descr)." (".trim($addinfo).")</li>";
 }
-echo "</ul>";
+echo "</ul></td>";
+$lines=file('instr.a');
+echo "<td style='width: 60px'>&nbsp;</td>";
+echo "<td><ul>";
+for ($i=0; $i<count($lines); $i++) {
+		list($link, $descr, $addinfo)=explode('==>', $lines[$i]);
+		echo "<li><a href=".trim($link).">".$link."</a> - ".trim($descr)." (".trim($addinfo).")</li>";
+}
+echo "</ul></td></tr>";
+echo "</Table>";
 echo "</BODY></HTML>";
 ?>
--- www/test1.php
+++
@@ -1,6 +0,0 @@
-<?php
-echo "<html><head><title>STxt</title></head><body>\n<form action=writ.php method=post name=frm1>\n";
-echo "<input type=text name=param> <input type=submit value=send>\n";
-echo "</form>\n</body></html>\n";
-echo "<!-- endfile -->";
-?>
